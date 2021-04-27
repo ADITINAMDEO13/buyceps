b32109ef45e9a343e5e7ba9c08d025359bc22d84
Namdev <adnamdev@deloitte.com>
2021-04-27 14:29:25 +0530

    first commit

--- components/SearchBar/index.jsx
+++ components/SearchBar/index.jsx
@@ -1,6 +1,19 @@
 import React, { useEffect, useState } from "react";
 
 export default function SearchBar(props) {
+	const { setTitle, setYear, setId } = props;
+
+	const [titleInfo, setTitleInfo] = useState("");
+	const [yearInfo, setYearInfo] = useState("");
+	const [idInfo, setIdInfo] = useState("");
+
+	const handleClick = (event) => {
+		event.preventDefault();
+		setTitle(titleInfo);
+		setYear(yearInfo);
+		setId(idInfo);
+	}
+	
 
 	return (
 		<div className="relative shadow-lg my-10 p-5 rounded-md mx-5">
@@ -15,6 +28,7 @@ export default function SearchBar(props) {
 						className="py-2 text-sm col-span-2 md:col-auto text-gray-600 bg-gray-100 rounded-md px-2  focus:outline-none focus:ring focus:border-purple-600 focus:bg-white focus:text-gray-900"
 						placeholder="Search Title"
 						autoComplete="off"
+						onChange={e => setTitleInfo(e.target.value)}
 					/>
 					<input
 		 				type="search"
@@ -22,6 +36,7 @@ export default function SearchBar(props) {
 						className="py-2 text-sm text-gray-600 bg-gray-100 rounded-md px-2  focus:outline-none focus:ring focus:border-purple-600 focus:bg-white focus:text-gray-900"
 						placeholder="Year"
 						autoComplete="off"
+						onChange={e => setYearInfo(e.target.value)}
 					/>
 					<input
 						type="search"
@@ -29,10 +44,12 @@ export default function SearchBar(props) {
 						className="py-2 text-sm  text-gray-600 bg-gray-100 rounded-md px-2  focus:outline-none focus:ring focus:border-purple-600 focus:bg-white focus:text-gray-900"
 						placeholder="ID"
 						autoComplete="off"
+						onChange={e => setIdInfo(e.target.value)}
 					/>
 					<button
 						type="submit"
-						className=" search-btn text-white font-bold p-1 rounded col-span-2 md:col-auto bg-yellow-400 focus:outline-none focus:shadow-outline">
+						className=" search-btn text-white font-bold p-1 rounded col-span-2 md:col-auto bg-yellow-400 focus:outline-none focus:shadow-outline"
+						onClick={handleClick}>
 						Search
 					</button>
 				</div>
--- components/SearchPage/index.jsx
+++ components/SearchPage/index.jsx
@@ -9,29 +9,39 @@ export default function SearchPage(props) {
 	const [year, setYear] = useState(2015);
 	const [id, setId] = useState("t2372222");
 	const [mydata, setMydata] = useState([]);
-	const fetchInfo = async() => {
+	
+	const fetchInfo = async () => {
         const result = await fetch(
-            `http://www.omdbapi.com/?apikey=592b0ff4&s=${title}&y=${year}&i=${id}`
+            `http://www.omdbapi.com/?apikey=592b0ff4&s=${title}&y=${year}&i=${id}&plot=full`
         );
         const data = await result.json();
 		setMydata(data.Search)
-		console.log(data.Search)
+		
         
     }
-	console.log(Data.Search)
+
 	useEffect(() => {
 		fetchInfo();
-	},[year,title,id])
+	}, [year, title, id])
+	
+
 	return (
 		<>
 			<p className="text-center my-10 text-3xl font-medium">Explore movies</p>
 			<div className="boxed">
-				<SearchBar />
-				{mydata.map((items) => {
-					return (
-						<h1>{ items.Title }</h1>
+				<SearchBar setTitle={setTitle} setYear={setYear} setId={ setId }/>
+				<div className="itemDesign">
+					{mydata.map((items) => {
+						return (
+							<div className="card">
+								<div className="poster">
+									<img src={ items.Poster}/>
+								</div>
+								<h1>{items.Title}</h1>
+								</div>
 					)
 				})}
+					</div>
 
 			</div>
 		</>
